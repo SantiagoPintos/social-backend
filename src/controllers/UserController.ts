1839564e566e4a2e31855d89ccd569dcd328e447
Santiago Pintos <spintos@outlook.es>
2024-03-21 13:19:39 -0300

    Validate user data before registration
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
 import { UserService } from "./../services/UserService";
+import { validateUserRegistration } from "./../validators/userValidator";
 
 const userService = new UserService();
 
 export const registerUser = async (req: Request, res: Response) => {
     try {
+        validateUserRegistration(req.body);
         await userService.register(req.body);
         res.status(201).json({ message: 'Usuario registrado con éxito.' });
     } catch (error: any) {
---
+++ src/validators/userValidator.ts
@@ -0,0 +1,46 @@
+interface UserToRegister{
+    name: string,
+    lastName: string,
+    username: string,
+    email: string,
+    password: string,
+}
+
+export function validateUserRegistration(data: UserToRegister):void{
+    if(!data.name || !data.lastName || !data.username || !data.email || !data.password){
+        throw new Error("All fields are required");
+    }
+    if(data.name.length < 3){
+        throw new Error("Name must be at least 3 characters long");
+    }
+    if(data.name.includes(" ")){
+        throw new Error("Name must not contain spaces");
+    }
+    if(data.lastName.length < 3){
+        throw new Error("Last name must be at least 3 characters long");
+    }
+    if(data.lastName.includes(" ")){
+        throw new Error("Last name must not contain spaces");
+    }
+    if(data.username.length < 3){
+        throw new Error("Username must be at least 3 characters long");
+    }
+    if(data.username.includes(" ")){
+        throw new Error("Username must not contain spaces");
+    }
+    if(data.password.length < 8){
+        throw new Error("Password must be at least 8 characters long");
+    }
+    if(data.email.length < 8){
+        throw new Error("Email must be at least 8 characters long");
+    }
+    if(!data.email.includes("@")){
+        throw new Error("Email must contain @");
+    }
+    if(!data.email.includes(".")){
+        throw new Error("Email must contain .");
+    }
+    if(data.email.includes(" ")){
+        throw new Error("Email must not contain spaces");
+    }
+}
