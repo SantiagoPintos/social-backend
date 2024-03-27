2823bafb2d93da7c465b58639b3e0b95262cc8ef
Santiago Pintos <spintos@outlook.es>
2024-03-27 18:46:31 -0300

    PostService: Refractor
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

--- src/services/PostService.ts
+++ src/services/PostService.ts
@@ -1,15 +1,18 @@
 import { AppDataSource } from "./../orm/dataSource";
 import { Post } from "./../entities/Post";
+import { FindManyOptions } from "typeorm";
 
 class PostService{
-    async getUserPosts(userId: number, numberOfPosts?: number) : Promise<Post[]> {
+    async getUserPosts(userId: number, numberOfPosts: number|undefined) : Promise<Post[]> {
         if(!userId){
             throw new Error('invalid user id');
         }
+        const options: FindManyOptions<Post> = { where: { autorId: userId } };
         if(numberOfPosts && numberOfPosts > 0){
-            return await AppDataSource.getRepository(Post).find({ where: { autorId: userId }, take: numberOfPosts, order: { id: 'DESC' }});
+            options.take = numberOfPosts;
+            options.order = { id: 'DESC' };
         }
-        return await AppDataSource.getRepository(Post).find({ where: { autorId: userId } });
+        return await AppDataSource.getRepository(Post).find(options);
     }
 }
 
