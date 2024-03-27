ca7cb598fbc443be997a80a5dab2bb67b0d0dfa1
Santiago Pintos <spintos@outlook.es>
2024-03-26 22:14:02 -0300

    PublicationController: check if the auth token is valid before returning the posts
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

--- src/controllers/PublicationController.ts
+++ src/controllers/PublicationController.ts
@@ -1,8 +1,10 @@
 import { Response, Request } from "express";
 import AuthService from "./../services/AuthService";
 import PostService from "./../services/PostService";
+import TokenService from "./../services/TokenService";
 import { Post } from "./../entities/Post";
 import { JwtPayload } from "jsonwebtoken";
+import { Tokens } from "./../entities/Tokens";
 
 export async function getUserPosts(req: Request, res: Response): Promise<void> {
     try {
@@ -10,13 +12,18 @@ export async function getUserPosts(req: Request, res: Response): Promise<void> {
         if (!token) {
             throw new Error('You are not authorized to access this resource');
         }
+        
         const payload: JwtPayload | string = AuthService.verifyToken(token);
         if (!payload) {
             throw new Error('You are not authorized to access this resource');
         }
         const userId = typeof payload === 'string' ? '' : payload.id;
+        const userToken: Tokens | null = await TokenService.getToken(userId);
+        if (token !== userToken?.token){
+            throw new Error('You are not authorized to access this resource');
+        } 
         const posts: Post[] = await PostService.getUserPosts(userId);
-        res.status(200).json(posts);
+        res.status(200).json({posts});
     } catch (error: unknown) {
         res.status(401).json({ message: (error as Error).message });
     }    
