c1e198a9d37d6eb22289bb5d22461bdcf6146cfb
Santiago Pintos <spintos@outlook.es>
2024-03-27 18:36:21 -0300

    Support 'numberOfPosts' as query parameter for publications endpoint
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

--- src/controllers/PublicationController.ts
+++ src/controllers/PublicationController.ts
@@ -12,7 +12,6 @@ export async function getUserPosts(req: Request, res: Response): Promise<void> {
         if (!token) {
             throw new Error('You are not authorized to access this resource');
         }
-        
         const payload: JwtPayload | string = AuthService.verifyToken(token);
         if (!payload) {
             throw new Error('You are not authorized to access this resource');
@@ -21,9 +20,10 @@ export async function getUserPosts(req: Request, res: Response): Promise<void> {
         const userToken: Tokens | null = await TokenService.getToken(userId);
         if (token !== userToken?.token){
             throw new Error('You are not authorized to access this resource');
-        } 
-        const posts: Post[] = await PostService.getUserPosts(userId);
-        res.status(200).json({posts});
+        }
+        const numberOfPosts = parseInt(req.query.numberOfPosts as string) || undefined;
+        const posts: Post[] = await PostService.getUserPosts(userId, numberOfPosts);
+        res.status(200).json({ posts });
     } catch (error: unknown) {
         console.log(error);
         res.status(401).json({ message: 'Something went wrong' });
--- src/services/PostService.ts
+++ src/services/PostService.ts
@@ -2,9 +2,12 @@ import { AppDataSource } from "./../orm/dataSource";
 import { Post } from "./../entities/Post";
 
 class PostService{
-    async getUserPosts(userId: number) : Promise<Post[]> {
+    async getUserPosts(userId: number, numberOfPosts?: number) : Promise<Post[]> {
         if(!userId){
             throw new Error('invalid user id');
+        }
+        if(numberOfPosts && numberOfPosts > 0){
+            return await AppDataSource.getRepository(Post).find({ where: { autorId: userId }, take: numberOfPosts, order: { id: 'DESC' }});
         }
         return await AppDataSource.getRepository(Post).find({ where: { autorId: userId } });
     }
