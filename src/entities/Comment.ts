1d0bedee7c2ce5905989f1695c3a997c4b36840f
Santiago Pintos <spintos@outlook.es>
2024-03-31 11:20:35 -0300

    Entities: Add relationship between Post and Comment
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

--- src/entities/Comment.ts
+++ src/entities/Comment.ts
@@ -1,8 +1,12 @@
-import { Entity, Column } from "typeorm";
+import { Entity, Column, ManyToOne } from "typeorm";
 import { Publication } from "./Publication";
+import { Post } from "./Post";
 
 @Entity()
 export class Comment extends Publication {
   @Column()
   parentPostId!: number;
+
+  @ManyToOne(() => Post, post => post.comments)
+  post!: Post;
 }
--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,6 +1,9 @@
-import { Entity } from "typeorm";
+import { Entity, OneToMany } from "typeorm";
 import { Publication } from "./Publication";
+import { Comment } from "./Comment";
 
 @Entity()
 export class Post extends Publication {
+    @OneToMany(() => Comment, comment => comment.post)
+    comments!: Comment[];
 }
