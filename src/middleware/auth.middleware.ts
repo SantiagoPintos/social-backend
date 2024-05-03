c15a59424d46de672fbcd83826724e1a4e48e039
Santiago Pintos <spintos@outlook.es>
2024-05-03 13:58:09 -0300

    Create a middleware to manage user authentication
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express';
+import AuthService from './../services/AuthService';
+import TokenService from './../services/TokenService';
+import { Tokens } from './../entities/Tokens';
+import NotAuthorizedError from './../errors/Publication/NotAuthorizedError';
+
+export async function authUser(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+        const token = req.headers.authorization;
+        if (!token) {
+            throw new NotAuthorizedError();
+        }
+        const payload = AuthService.verifyToken(token);
+        if (!payload) {
+            throw new NotAuthorizedError();
+        }
+        const userId = typeof payload === 'string' ? '' : payload.id;
+        const userToken: Tokens | null = await TokenService.getToken(userId);
+        if (token !== userToken?.token) {
+            throw new NotAuthorizedError();
+        }
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Unauthorized' });
+    }
+}
