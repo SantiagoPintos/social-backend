f9b3f3078ffe96ae55b18fad1e619b92ed96b8dd
Santiago Pintos <spintos@outlook.es>
2024-05-14 08:02:20 -0300

    Update postDTO

--- src/dtos/post.dto.ts
+++ src/dtos/post.dto.ts
@@ -1,7 +1,18 @@
+import { likeDTO } from "./like.dto"
+
+
 interface newPostDTO{
     autorId: number,
     content: string,
-    date: Date,
+    date: Date
 }
 
-export { newPostDTO }
+interface postDTO {
+    id: number,
+    autorId: number,
+    content: string,
+    date: Date,
+    likes: likeDTO[]
+}
+
+export { newPostDTO, postDTO }
