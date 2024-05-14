c6ca810d5e72d8af12a2f883368715517a620691
Santiago Pintos <spintos@outlook.es>
2024-05-13 22:52:29 -0300

    Implement likePost endpoint in LikeController

---
+++ src/controllers/LikeController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import LikeService from '@/services/LikeService';
+import { Like } from '@/entities/Like';
+
+export async function likePost(req: Request, res: Response): Promise<void> {
+    try {
+        const userId = parseInt(req.body.userId as string);
+        const { postId, type } = req.params;  
+        if (!userId || !postId || (type !== 'post' && type !== 'comment')) {
+            throw new Error('invalid ids');
+        }
+        const like: Like = await LikeService.likePublication(userId, parseInt(postId), type);
+        res.status(200).json({ message: 'Like created successfully', like });
+    } catch (error: unknown) {
+        console.log((error as Error).message);
+        res.status(401).json({ message: 'Something went wrong' });
+    }
+}
+
