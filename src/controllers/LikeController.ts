0501afa2650ebd6a906d578a53ea32593093768f
Santiago Pintos <spintos@outlook.es>
2024-05-17 16:14:20 -0300

    refactor: Update LikeController to handle custom error classes and improve error handling

--- src/controllers/LikeController.ts
+++ src/controllers/LikeController.ts
@@ -1,19 +1,29 @@
 import { Request, Response } from 'express';
 import LikeService from '@/services/LikeService';
-import { likeDTO } from '@/dtos/like.dto';
+import idError from '@/errors/IdError';
 
 export async function likePost(req: Request, res: Response): Promise<void> {
     try {
         const userId = parseInt(req.body.userId as string);
         const { postId, type } = req.params;  
         if (!userId || !postId || (type !== 'post' && type !== 'comment')) {
-            throw new Error('invalid ids');
+            throw new idError('invalid ids');
         }
-        const like: likeDTO = await LikeService.likePublication(userId, parseInt(postId), type);
-        res.status(200).json({ message: 'Like created successfully', like });
+        const result = await LikeService.likePublication(userId, parseInt(postId), type);
+        if (result.like) {
+            res.status(200).json({ message: result.message, like: result.like });
+        } else {
+            res.status(200).json({ message: result.message });
+        }
     } catch (error: unknown) {
-        console.log((error as Error).message);
-        res.status(401).json({ message: 'Something went wrong' });
+        if ((error as Error).name === 'PostError' || (error as Error).name === 'CommentError') {
+            res.status(404).json({ message: (error as Error).message });
+        } else if ((error as Error).name === 'idError') {
+            res.status(400).json({ message: (error as Error).message });
+        } else {
+            console.log((error as Error).message);
+            res.status(500).json({ message: 'Something went wrong' });
+        }
     }
 }
 
