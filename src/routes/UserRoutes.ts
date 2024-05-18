b3bb31df64c32424b1ef830a54f2ec47fc476799
Santiago Pintos <spintos@outlook.es>
2024-05-18 20:12:56 -0300

    refactor: Add deleteProfileImage route for handling profile image deletion

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { registerUser, loginUser } from "@/controllers/UserController";
-import { uploadProfileImage } from "@/controllers/UserController";
+import { uploadProfileImage, deleteProfileImage } from "@/controllers/UserController";
 import { authUser } from "@/middleware/auth.middleware";
 import { userIdValidator } from "@/middleware/userIdValidator.middleware";
 import { upload } from "@/middleware/storage.middleware";
@@ -9,5 +9,6 @@ const router = express.Router();
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/:id/profile-image', authUser, userIdValidator, upload.single('profileImage'), uploadProfileImage);
+router.delete('/:id/profile-image', authUser, userIdValidator, deleteProfileImage);
 
 export default router;
