20e6ec018221fa6dd7ff8a748d94b4c9df592a1b
Santiago Pintos <spintos@outlook.es>
2024-05-18 19:26:23 -0300

    refactor: Add route for uploading profile image

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import { registerUser, loginUser } from "@/controllers/UserController";
+import { uploadProfileImage } from "@/controllers/UserController";
+import { authUser } from "@/middleware/auth.middleware";
+import { userIdValidator } from "@/middleware/userIdValidator.middleware";
+import { upload } from "@/middleware/storage.middleware";
 
 const router = express.Router();
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.post('/:id/profile-image', authUser, userIdValidator, upload.single('profileImage'), uploadProfileImage);
 
 export default router;
