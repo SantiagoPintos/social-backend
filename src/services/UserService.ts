144024d41acce8b78b2ef0daf7f8b79a017a48fc
Santiago Pintos <spintos@outlook.es>
2024-05-18 20:12:47 -0300

    feat: Delete user profile image when updating with a new image

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -2,6 +2,7 @@ import { AppDataSource } from "@/orm/dataSource";
 import { User } from "@/entities/User";
 import PasswordService from "./PasswordService";
 import UserError from "@/errors/User/UserError";
+import fs from 'fs';
 
 class UserService {
 
@@ -45,8 +46,33 @@ class UserService {
         try {
             if(id<=0) throw new UserError('Invalid user id');
             if(path.length<=6) throw new UserError('Invalid image path');
+            const user = await this.getUserById(id);
+            if(user.profileImage.length > 6) await this.deleteUserProfileImage(id);
             await AppDataSource.getRepository(User).update(id, { profileImage: path });
         } catch (error: unknown) {
+            throw new UserError('User not found');
+        }
+    }
+
+    async deleteUserProfileImage(id: number): Promise<void> {
+        try {
+            if(id<=0) throw new UserError('Invalid user id');
+            const user = await this.getUserById(id);
+            if (!user) {
+                throw new UserError('User not found');
+            }
+            try {
+                if(user.profileImage.length > 6) fs.unlinkSync(user.profileImage);
+            } catch (error: unknown) {
+                console.log('Image file not found');
+            }
+            try {
+                await AppDataSource.getRepository(User).update(id, { profileImage: '' });
+            } catch (error: unknown) {
+                console.log('Error updating user profile image');
+            }
+        } catch (error: unknown) {
+            console.log('error', error);
             throw new UserError('User not found');
         }
     }
