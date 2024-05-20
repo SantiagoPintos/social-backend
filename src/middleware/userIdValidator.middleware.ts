3ba4044cba66bec9d72fcc658a30da2ac1a814c5
Santiago Pintos <spintos@outlook.es>
2024-05-20 15:16:33 -0300

    refactor: Update userIdValidator middleware to use userMapper for UserDTO mapping

--- src/middleware/userIdValidator.middleware.ts
+++ src/middleware/userIdValidator.middleware.ts
@@ -1,15 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 import UserService from '@/services/UserService';
-import { User } from '@/entities/User';
+import userMapper from '@/mappers/userMapper';
+import { UserDTO } from '@/dtos/user.dto';
 
 export async function userIdValidator(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
         const userId = req.params.id;
         if (!userId) res.status(400).json({ message: 'Invalid user id' });
         if (isNaN(parseInt(userId))) res.status(400).json({ message: 'Invalid user id' });
-        const user = await UserService.getUserById(parseInt(userId));
-        //TODO: use userDTO instead of User
-        (req as Request & { user: User }).user = user;
+        const user = userMapper.toDto(await UserService.getUserById(parseInt(userId)));
+        (req as Request & { user: UserDTO }).user = user;
         next(); 
     } catch (error: unknown) {
         console.log((error as Error).message);
