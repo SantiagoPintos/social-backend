696210e98da6c87ae56039111f1c4a0dedc582f5
Santiago Pintos <spintos@outlook.es>
2024-05-22 21:45:40 -0300

    refactor: Update UserRoutes to use consistent profile image endpoints

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -7,8 +7,8 @@ import { upload } from "@/middleware/storage.middleware";
 const router = express.Router();
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.post('/:id/profile-image', authUser, userIdValidator, upload.single('profileImage'), uploadProfileImage);
-router.delete('/:id/profile-image', authUser, userIdValidator, deleteProfileImage);
+router.post('/profile-image', authUser, userIdValidator, upload.single('profileImage'), uploadProfileImage);
+router.delete('/profile-image', authUser, userIdValidator, deleteProfileImage);
 router.post('/follow/:id', authUser, userIdValidator, followUser);
 
 export default router;
