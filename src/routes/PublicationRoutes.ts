a2e534b2f5431c1f99e2f58c40f2616475675146
Santiago Pintos <spintos@outlook.es>
2024-05-23 19:19:00 -0300

    chore: Update PublicationRoutes to include userIdValidator middleware

--- src/routes/PublicationRoutes.ts
+++ src/routes/PublicationRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { getUserPosts, newUserPost, newUserPostComment } from "@/controllers/PublicationController";
 import { authUser } from "@/middleware/auth.middleware";
+import { userIdValidator } from "@/middleware/userIdValidator.middleware";
 
 const router = express.Router();
-router.get('/', authUser, getUserPosts);
-router.post('/', authUser, newUserPost);
-router.post('/comments', authUser, newUserPostComment);
+router.get('/', authUser, userIdValidator, getUserPosts);
+router.post('/', authUser, userIdValidator, newUserPost);
+router.post('/comments', authUser, userIdValidator, newUserPostComment);
 
 export default router;
