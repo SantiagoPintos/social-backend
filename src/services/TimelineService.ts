bcebcd4c5e0fc230e96983165c8f294a84411db7
Santiago Pintos <spintos@outlook.es>
2024-05-24 13:44:37 -0300

    refactor: Update getTimeline method in TimelineService to return an array of postDTO objects

--- src/services/TimelineService.ts
+++ src/services/TimelineService.ts
@@ -1,15 +1,16 @@
 import UserError from "@/errors/User/UserError";
 import UserService from "./UserService";
 import PostService from "./PostService";
+import { postDTO } from "@/dtos/post.dto";
 
 class TimelineSerivice{
-    async getTimeline(userId: number){
+    async getTimeline(userId: number): Promise<postDTO[]>{
         if(!userId) throw new UserError('Invalid user');
 
         const user = await UserService.getUserById(userId);
         if(!user) throw new UserError('User not found');
 
-        const followed = await UserService.getListOfFollowers(userId);
+        const followed = await UserService.getListOfFolloweds(userId);
         if(followed.length === 0) throw new UserError('No followers found');
         
         const followedIds = followed.map(follower => follower.id);
