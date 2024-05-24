b6df0097b384809f4241307a51e294ad520d5d6d
Santiago Pintos <spintos@outlook.es>
2024-05-24 13:55:37 -0300

    Storage middleware: fix uploads path

--- src/middleware/storage.middleware.ts
+++ src/middleware/storage.middleware.ts
@@ -2,15 +2,15 @@ import multer from 'multer';
 import path from 'path'
 import fs from 'fs';
 
-const uploadDir = path.join(__dirname, './../uploads');
-const profilesDir = path.join(__dirname, './../uploads/profile_images');
+const uploadDir = path.join(__dirname, './../../uploads');
+const profilesDir = path.join(__dirname, './../../uploads/profile_images');
 
 if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
 if (!fs.existsSync(profilesDir)) fs.mkdirSync(profilesDir);
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, './../uploads/profile_images'));
+        cb(null, path.join(__dirname, './../../uploads/profile_images'));
     },
     filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
