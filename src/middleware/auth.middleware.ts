1d7939694e489d3ea2cea7ae4d5a6ae9c232e714
Santiago Pintos <spintos@outlook.es>
2024-06-22 19:06:32 -0300

    chore: Update auth middleware to handle token verification errors

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -5,13 +5,21 @@ import NotAuthorizedError from '@/errors/Publication/NotAuthorizedError';
 export async function authUser(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
         const token = req.headers.authorization;
-        if (!token) {
+        if (!token) throw new NotAuthorizedError("Unauthorized");
+        let payload;
+        try{
+            payload = AuthService.verifyToken(token);
+        } catch (error: unknown) {
             throw new NotAuthorizedError("Unauthorized");
         }
-        const payload = AuthService.verifyToken(token);
-        const userId = typeof payload === 'string' ? '' : payload.id;
+        if (!payload || typeof payload !== 'object' || !payload.id) throw new NotAuthorizedError("Unauthorized");
+        const userId = payload.id;
         next(userId);
     } catch (error) {
-        res.status(401).json({ message: 'Unauthorized' });
+        if ((error as Error).name == 'NotAuthorizedError') {
+            res.status(401).json({ message: (error as Error).message});
+        } else {
+            res.status(500).json({ message: 'Internal Server Error' });
+        }
     }
 }
