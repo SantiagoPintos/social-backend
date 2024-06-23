cde6a325f029ac21c102290443cfa9324703b693
Santiago Pintos <spintos@outlook.es>
2024-06-23 19:35:34 -0300

    refactor: Update postMapper to use async/await for user retrieval

--- src/mappers/postMapper.ts
+++ src/mappers/postMapper.ts
@@ -2,11 +2,14 @@ import { Post } from "@/entities/Post";
 import { postDTO, postToTimelineDTO } from "@/dtos/post.dto";
 import { likeDTO } from "@/dtos/like.dto";
 import { CommentDTO } from "@/dtos/comment.dto";
-import userMapper from "./userMapper";
 import UserService from "@/services/UserService";
+import { UserToPostDTO } from "@/dtos/user.dto";
+import userMapper from "./userMapper";
 
 export class postMapper{
-    toDto(post: Post): postDTO{
+    async toDto(post: Post): Promise<postDTO>{
+        const autorDTO: UserToPostDTO =  userMapper.toTimelineDTO(await UserService.getUserById(post.autorId));
+
         const dto: postDTO = {
             id: post.id,
             autorId: post.autorId,
@@ -20,12 +23,12 @@ export class postMapper{
                 }
                 return like;
             }),
-            comments: post.comments?.map((c) => {
+            comments: await Promise.all(post.comments?.map(async (c) => {
                 const comment: CommentDTO = {
                     id: c.id,
                     content: c.content,
                     date: c.date,
-                    autorId: c.autorId,
+                    autorId: autorDTO,
                     parentPostId: c.parentPost.id,
                     likes: c.likes.map((l) => {
                         const like: likeDTO = {
@@ -37,17 +40,18 @@ export class postMapper{
                     })
                 }
                 return comment;
-            }) ?? []
-            
+            }) ?? [])
         }
 
         return dto;
     }
 
     async toTimelineDTO(post: Post): Promise<postToTimelineDTO>{
+        const autorDTO:UserToPostDTO = userMapper.toTimelineDTO(await UserService.getUserById(post.autorId));
+
         const dto: postToTimelineDTO = {
             id: post.id,
-            autor: userMapper.toTimelineDTO(await UserService.getUserById(post.autorId)),
+            autor: autorDTO,
             content: post.content,
             date: post.date,
             likes: post.likes?.map((l) => {
@@ -63,7 +67,7 @@ export class postMapper{
                     id: c.id,
                     content: c.content,
                     date: c.date,
-                    autorId: c.autorId,
+                    autorId: autorDTO,
                     parentPostId: c.parentPost.id,
                     likes: c.likes?.map((l) => {
                         const like: likeDTO = {
@@ -75,8 +79,7 @@ export class postMapper{
                     })
                 }
                 return comment;
-            }) ?? []
-            
+            }) ?? [] 
         }
 
         return dto;
