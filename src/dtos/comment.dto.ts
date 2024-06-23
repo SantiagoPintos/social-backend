1cc7d69915604ee7eb3b8889d8e054a0df4e84d2
Santiago Pintos <spintos@outlook.es>
2024-06-23 19:27:51 -0300

    feat: Update CommentDTO to include UserToPostDTO

--- src/dtos/comment.dto.ts
+++ src/dtos/comment.dto.ts
@@ -1,8 +1,9 @@
 import { likeDTO } from "./like.dto"
+import { UserToPostDTO } from "./user.dto"
 
 interface CommentDTO{
     id: number,
-    autorId: number,
+    autorId: UserToPostDTO,
     content: string,
     date: Date,
     likes: likeDTO[],
