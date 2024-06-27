9fd4b6fe32ba2ce06001c700d98c612cf8b8bf99
Santiago Pintos <spintos@outlook.es>
2024-06-27 10:44:59 -0300

    chore: Add userIdValidator middleware to LikeRoutes

--- src/routes/LikeRoutes.ts
+++ src/routes/LikeRoutes.ts
@@ -1,8 +1,9 @@
 import Router from "express";
 import { likePost } from "@/controllers/LikeController";
 import { authUser } from "@/middleware/auth.middleware";
+import { userIdValidator } from "@/middleware/userIdValidator.middleware";
 
 const router = Router();
-router.post('/:type/:postId', authUser, likePost);
+router.post('/:type/:postId', authUser, userIdValidator, likePost);
 
 export default router;
