33c8ec5131f9f114095be13d397bb16f95320dce
Santiago Pintos <spintos@outlook.es>
2024-07-17 13:49:45 -0300

    refactor: Remove specific TypeORM code from Post entity

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,13 +1,14 @@
-import { Entity, OneToMany } from "typeorm";
 import { Publication } from "./Publication";
 import { Comment } from "./Comment";
 import { Like } from "./Like";
 
-@Entity()
 export class Post extends Publication {
-    @OneToMany(() => Comment, (comment) => comment.parentPost)
-    comments: Comment[] | undefined
-
-    @OneToMany(() => Like, (like) => like.post)
+    comments: Comment[]
     likes!: Like[]
+
+    constructor(autor: number, content: string) {
+        super(autor, content);
+        this.comments = new Array<Comment>();
+        this.likes = new Array<Like>();
+    }
 }
