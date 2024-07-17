cf3d0829dffee1945bc959ff14680e420d12885b
Santiago Pintos <spintos@outlook.es>
2024-07-17 15:27:21 -0300

    refactor: Remove TypeORM specific code

--- src/entities/UserFollower.ts
+++ src/entities/UserFollower.ts
@@ -1,14 +1,13 @@
-import { Entity, PrimaryGeneratedColumn, ManyToOne } from "typeorm";
 import { User } from "./User";
 
-@Entity()
 export class UserFollower {
-  @PrimaryGeneratedColumn()
     id!: number;
-
-  @ManyToOne(() => User, (user) => user.followers)
     followed!: User;
-
-  @ManyToOne(() => User, (user) => user.following)
     follower!: User;
+
+    constructor(id: number, followed: User, follower: User) {
+        this.id = id;
+        this.followed = followed;
+        this.follower = follower;
+    }
 }
