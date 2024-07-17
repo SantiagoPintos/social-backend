767960008607b1ef3a66ff484763e5665ac93f58
Santiago Pintos <spintos@outlook.es>
2024-07-17 15:25:50 -0300

    refactor: Remove TypeORM specific code fro Like entity

--- src/entities/Like.ts
+++ src/entities/Like.ts
@@ -1,22 +1,19 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { User } from "./User";
 import { Post } from "./Post";
 import { Comment } from "./Comment";
 
-@Entity()
 export class Like{
-    @PrimaryGeneratedColumn()
     id!: number;
-
-    @Column()
     date!: Date;
-
-    @ManyToOne(() => User, (user) => user.likes)
     user!: User;
-
-    @ManyToOne(() => Post, { nullable: true })
     post!: Post;
-
-    @ManyToOne(() => Comment, { nullable: true })
     comment!: Comment;
+
+    constructor(id: number, date: Date, user: User, post: Post, comment: Comment) {
+        this.id = id;
+        this.date = date;
+        this.user = user;
+        this.post = post;
+        this.comment = comment;
+    }
 }
