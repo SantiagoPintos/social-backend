fc0f12a79e2872379c70dd83586e9c35088494dc
Santiago Pintos <spintos@outlook.es>
2024-07-17 13:23:37 -0300

    refactor: Remove TypeORM specific code from User entity

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,38 +1,29 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany, Unique } from "typeorm";
 import { Like } from "./Like";
 import { UserFollower } from "./UserFollower";
 
-@Entity()
 export class User {
-  @PrimaryGeneratedColumn()
     id!: number;
-
-  @Column()
     name!: string;
-
-  @Column()
     lastName!: string;
-
-  @Unique("unique_username", ["username"])
-  @Column()
     username!: string;
-
-  @Column()
     email!: string;
-
-  @Column()
     password!: string;
-
-  @Column()
     profileImage!: string;
-
-  @OneToMany(() => Like, (like) => like.user)
     likes!: Like[];
-
-  @OneToMany(() => UserFollower, userFollower => userFollower.followed)
     followers!: UserFollower[];
-
-  @OneToMany(() => UserFollower, userFollower => userFollower.follower)
     following!: UserFollower[];
+
+    constructor(id: number, name: string, lastName: string, username: string, email: string, password: string, profileImage: string) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.profileImage = profileImage;
+        this.likes = new Array<Like>();
+        this.followers = new Array<UserFollower>();
+        this.following = new Array<UserFollower>();
+    }
 }
 
